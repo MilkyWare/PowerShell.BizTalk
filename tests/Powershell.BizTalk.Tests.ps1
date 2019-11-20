2ad3388a15fcdc79149a105f2d39adcfae2a86a1
Cameron Cowen <cameronc94@gmail.com>
2019-11-20 00:47:17 +0000

    Added test for get hosts

--- tests/Powershell.BizTalk.Tests.ps1
+++ tests/Powershell.BizTalk.Tests.ps1
@@ -1,13 +1,31 @@
 #Requires -Modules Pester
 
-Describe "Create InProcess Host" {
+Describe "Create and delete in-process host" {
     BeforeAll {
         Import-Module ".\src\PowerShell.BizTalk\PowerShell.BizTalk.psd1" -Force
     }
-    Context "Create In-Process host" {
+    It "Create Host" {
         New-BTSHost -Name "TestInProcess" -HostType InProcess -GroupName "BizTalk Application Users"
     }
-    Context "Create Isolated host" {
-        New-BTSHost -Name "TestIsolated" -HostType Isolated -GroupName "BizTalk Application Users"
+    Context "Cleanup" {
+        Remove-BTSHost -Name "TestInProcess"
+    }
+}
+
+Describe "Create and delete isolated host" {
+    BeforeAll {
+        Import-Module ".\src\PowerShell.BizTalk\PowerShell.BizTalk.psd1" -Force
+    }
+    It "Create Host" {
+        New-BTSHost -Name "TestIsolated" -HostType Isolated -GroupName "BizTalk Isolated Host Users"
+    }
+    Context "Cleanup" {
+        Remove-BTSHost -Name "TestIsolated"
+    }
+}
+
+Describe "Get hosts" {
+    It "Get list of hosts" {
+        Get-BTSHost
     }
 }
